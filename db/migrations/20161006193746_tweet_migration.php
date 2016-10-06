d1e40c98da0052d71728c485bbb9798db39d2a95
Glenn Eggleton <geggleto@gmail.com>
2016-10-06 16:05:18 -0400

    Added composer short-cuts for phynx

--- db/migrations/20161006193746_tweet_migration.php
+++ db/migrations/20161006193746_tweet_migration.php
@@ -4,33 +4,12 @@ use Phinx\Migration\AbstractMigration;
 
 class TweetMigration extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function up()
     {
         $table = $this->table('tweets');
         $table->addColumn('owner_id', 'integer')
             ->addColumn('contents', 'string', array('limit' => 140))
-            ->addIndex(array('user_id'))
+            ->addIndex(array('owner_id'))
             ->save();
     }
 }
