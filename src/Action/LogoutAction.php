39edb88676c9a71812229980b6ef688d4309e2e1
Glenn Eggleton <geggleto@gmail.com>
2016-10-07 11:44:54 -0400

    Added logout action

--- src/Action/LogoutAction.php
+++ src/Action/LogoutAction.php
@@ -11,12 +11,21 @@ namespace Twitter\Action;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Twitter\Services\SessionService;
 
 class LogoutAction extends Action
 {
+    protected $sessionService;
+
+    public function __construct(SessionService $sessionService)
+    {
+        $this->sessionService = $sessionService;
+    }
 
     public function __invoke(Request $request, Response $response, array $args)
     {
-        // TODO: Implement __invoke() method.
+        $this->sessionService->destroy();
+
+        return $response->withJson(['result' => 'true']);
     }
 }
